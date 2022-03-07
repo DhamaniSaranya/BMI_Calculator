3b478d075ba82c962c7f8ec133693238c0d5adbc
DhamaniSaranya <dhamanisaranya69@gmail.com>
2022-03-07 15:51:55 +0530

    Assignment5

--- assignments/play_with_array/play_with_array.js
+++ assignments/play_with_array/play_with_array.js
@@ -7,6 +7,10 @@ function getEven(arr) {
 
         Write you code below
     */
+    const even_num = arr.filter(num => {
+        return num % 2 === 0;
+    });
+        return (even_num);
 
 }
 
@@ -18,6 +22,8 @@ function multiplyByN(arr, n) {
             Output: [3,9,13,165]
         Write you code below
     */
+    multiply = arr.map((x) => x * n);
+    return multiply;
 }
 
 function removeNthElement(arr, n) {
@@ -28,6 +34,8 @@ function removeNthElement(arr, n) {
             Output: [1,3,4,7]
         Write you code below
     */
+    arr.splice(n, 1);
+    return arr;
 }
 
 module.exports = {
